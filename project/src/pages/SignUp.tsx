77ec66f7166a4660bf5cbb3968374f61afa39403
Mayor003 <152872559+Mayor003@users.noreply.github.com>
2025-03-24 11:53:59 +0100

    adjusted button

--- project/src/pages/SignUp.tsx
+++ project/src/pages/SignUp.tsx
@@ -188,7 +188,7 @@ const SignUp = () => {
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
               >
                 Sign Up
               </button>
